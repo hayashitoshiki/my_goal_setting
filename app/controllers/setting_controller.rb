77505709d27b055472b312c00212a36af485a9a1
hayashitoshiki <s1521093@cco.kanagawa-it.ac.jp>
2018-04-06 23:47:31 +0900

    4月6日変更点、短期スパンたたき台

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -1,4 +1,10 @@
 class SettingController < ApplicationController
   def setting_top
   end
+
+  def goal_set
+  end
+
+  def day_setting
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'list/top' => 'list#list_top'
 
+  get 'setting/day' => 'setting#day_setting'
+  get 'setting/set' => 'setting#goal_set'
   get 'setting/top' => 'setting#setting_top'
 
   get '/' => 'home#top'
