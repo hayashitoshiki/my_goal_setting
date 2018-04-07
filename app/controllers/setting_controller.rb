b1f9ea43cc1011b4de9e9862939b2e84a6239c2a
hayashitoshiki <s1521093@cco.kanagawa-it.ac.jp>
2018-04-07 22:48:17 +0900

    4月7日,短期スパン、フロント仮完成

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -1,4 +1,6 @@
 class SettingController < ApplicationController
+  require "date"
+
   def setting_top
   end
 
@@ -6,5 +8,16 @@ class SettingController < ApplicationController
   end
 
   def day_setting
+    today = Date.today
+    sa = (Date.strptime(params[:goal_day],'%Y-%m-%d') - today).numerator
+    @dates = []
+
+    for i in 1..sa do
+        @dates[i-1] = today.strftime("%m月%d日")
+        today += 1
+      end
+
+    @goal_final = params[:goal_final]
+    @goal_day =(Date.strptime(params[:goal_day],'%Y-%m-%d')).strftime("%m月%d日")
   end
 end
